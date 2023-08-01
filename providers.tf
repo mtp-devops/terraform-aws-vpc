ce9f7ea74e3eed1abd73b1db46b5394679022d68
Gil Fridman <89933538+Gil202@users.noreply.github.com>
2023-08-01 18:02:29 +0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -12,7 +12,9 @@ provider "aws" {
 
 terraform {
   # The configuration for this backend will be filled in by Terragrunt
-  backend "s3" {}
+  backend "s3" {
+  skip_region_validation = true
+  }
 
   # The latest version of Terragrunt (v0.19.0 and above) requires Terraform 0.12.0 or above.
   required_version = ">= 0.12.0"
